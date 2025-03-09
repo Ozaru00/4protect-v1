db61608b7faa8a22c2bde5c4036da1a7f3b11846
Ozaru00 <124617767+Ozaru00@users.noreply.github.com>
2025-03-09 09:26:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1077978494098751590', // Buyer
-        couleur: '#6495ED', // Couleur HEX
+        couleur: '#000000', // Couleur HEX
         footer: 'Ozaru Protect', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '5', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
